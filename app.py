7905a88f9568be4ebe9f04aae5d83cdf1f8c8e18
varun-mangla <iamvarunmangla@gmail.com>
2024-04-12 01:18:25 +0530

    test

--- app.py
+++ app.py
@@ -2,3 +2,13 @@ import streamlit as st
 st.title ('Startup Dashboard')
 st.header('Learning streamlit')
 st. write('This is normal text')
+
+col1, col2, col3,col4=st.columns(4)
+with col1:
+    st.header('Hello')
+with col2:
+    st.header('World')
+with col3:
+    st.header('this')
+with col4:
+    st.header('is Varun')
