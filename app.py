19058a80be70d80d86ee8259a8311215aa982c56
varun-mangla <iamvarunmangla@gmail.com>
2024-04-12 01:51:58 +0530

    Basic framework

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-st.title ('Startup Dashboard')
-st.header('Learning streamlit')
-st. write('This is normal text')
+st.info('Site is under construction!.....Please be with me')
 
-col1, col2, col3,col4=st.columns(4)
-with col1:
-    st.header('Hello')
-with col2:
-    st.header('World')
-with col3:
-    st.header('this')
-with col4:
-    st.header('is Varun')
+st.sidebar.title('Startup Funding Analysis')
+
+option= st. sidebar.selectbox('Select One', ['Overall Analysis','Startup','Investor'])
+if option=='Overall Analysis':
+    st.title('Overall Analysis')
+elif option=='Startup':
+    st.title('Startup Analysis')
+    st.sidebar.selectbox('Select Startup',['Flipkart','Ola'])
+elif option =='Investor':
+    st.title('Investor Analysis')
+    st.sidebar.selectbox('Select Investor', ['aamir 1', 'aamir 2'])
