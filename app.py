4ac95b9954bf6c346f24a5a5bdd8776cd2188f04
varun-mangla <iamvarunmangla@gmail.com>
2024-04-12 02:03:39 +0530

    Syntax Correction

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ st.info('Site is under construction!.....Please be with me')
 
 st.sidebar.title('Startup Funding Analysis')
 
-option= st. sidebar.selectbox('Select One', ['Overall Analysis','Startup','Investor'])
-if option=='Overall Analysis':
+option = st. sidebar.selectbox('Select One', ['Overall Analysis','Startup','Investor'])
+if option == 'Overall Analysis':
     st.title('Overall Analysis')
-elif option=='Startup':
+elif option == 'Startup':
     st.title('Startup Analysis')
     st.sidebar.selectbox('Select Startup',['Flipkart','Ola'])
-elif option =='Investor':
+elif option == 'Investor':
     st.title('Investor Analysis')
     st.sidebar.selectbox('Select Investor', ['aamir 1', 'aamir 2'])
